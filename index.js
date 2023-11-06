76b47f2e81dc68fcd70144079763b8af4c096d3d
ml1232 <134437061+ml1232@users.noreply.github.com>
2023-11-06 15:28:36 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import App from "./App";
+import "./index.css";
+import { createRoot } from "react-dom/client";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
